8c245cb973bb7409ce8507dfd75b95177b374bb7
palmtopj <palmtopj@uw.edu>
2023-06-08 09:02:21 -0700

    persistent navbar added

--- assistive-tech/front/src/App.js
+++ assistive-tech/front/src/App.js
@@ -2,15 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    // <Router>
-    //   <h1>Navbar</h1>
-    //   <hr />
-    //   <Routes>
-    //     <Route path="/" element={<h2>Home</h2>}/>
-    // </Routes>
-    <div className="App">
-      <h1>Alpha 1 - we hear you!</h1>
-    </div>
+    <Router>
+      <h1>Navbar</h1>
+      <hr />
+      <Routes>
+        <Route path="/" element={<h2>Home Page</h2>}/>
+    </Routes>
+    </Router>
   );
 }
 
