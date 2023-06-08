932f83b792599d510f0aad8f6bed338443da1335
palmtopj <palmtopj@uw.edu>
2023-06-08 08:47:42 -0700

    hello world on react

---
+++ assistive-tech/front/src/App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    // <Router>
+    //   <h1>Navbar</h1>
+    //   <hr />
+    //   <Routes>
+    //     <Route path="/" element={<h2>Home</h2>}/>
+    // </Routes>
+    <div className="App">
+      <h1>Alpha 1 - we hear you!</h1>
+    </div>
+  );
+}
+
+export default App;
