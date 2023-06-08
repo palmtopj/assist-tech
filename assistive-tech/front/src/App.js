345f5d00029b11f82f9bab23488023cc1bc30710
palmtopj <palmtopj@uw.edu>
2023-06-08 11:22:00 -0700

    homepage created

--- assistive-tech/front/src/App.js
+++ assistive-tech/front/src/App.js
@@ -1,18 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
+  // add stAtes here
   return (
     <Router>
       <h1>Navbar</h1>
       <hr />
       <Routes>
-        <Route path="/" element={<h2>assistive technologies</h2>}/>
-        <Route path="/login" element={<h2>login page</h2>}/>
-        <Route path="/signup" element={<h2>signup page</h2>}/>
-        <Route path="/user/:username" element={<h2>profile page</h2>}/>
-      
-        <Route path="/device/:id" element={<h2>device page</h2>}/>
-        <Route path="/*" element={<h2>404 page not found</h2>}/>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login" element={<h2>login page</h2>} />
+        <Route path="/signup" element={<h2>signup page</h2>} />
+        <Route path="/category/:id" element={<h2>devices page</h2>} />
+        <Route path="/device/:id" element={<h2>device page</h2>} />
+        <Route path="/*" element={<h2>404 page not found</h2>} />
     </Routes>
     </Router>
   );
---
+++ assistive-tech/front/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.css"
+
+export default function Home() {
+    return (
+        <main className="Home">
+            <h1>Assistive Technologies less than 250</h1>
+            <h2>Select a category</h2>
+        </main>
+    )
+}
