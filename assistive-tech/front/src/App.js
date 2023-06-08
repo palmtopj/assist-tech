a5029d0aa0aee5f3593ac7e41e6c2ce178bdb7e0
palmtopj <palmtopj@uw.edu>
2023-06-08 10:43:56 -0700

    6 routes added

--- assistive-tech/front/src/App.js
+++ assistive-tech/front/src/App.js
@@ -6,7 +6,13 @@ function App() {
       <h1>Navbar</h1>
       <hr />
       <Routes>
-        <Route path="/" element={<h2>Home Page</h2>}/>
+        <Route path="/" element={<h2>assistive technologies</h2>}/>
+        <Route path="/login" element={<h2>login page</h2>}/>
+        <Route path="/signup" element={<h2>signup page</h2>}/>
+        <Route path="/user/:username" element={<h2>profile page</h2>}/>
+      
+        <Route path="/device/:id" element={<h2>device page</h2>}/>
+        <Route path="/*" element={<h2>404 page not found</h2>}/>
     </Routes>
     </Router>
   );
