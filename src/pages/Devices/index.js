006cf13e036eb1ea8a766272d18670b31afd5fff
palmtopj <palmtopj@uw.edu>
2023-06-09 10:45:46 -0700

    cleaned directories

---
+++ src/pages/Devices/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./style.css"
+
+export default function Home() {
+    return (
+        <main className="Home">
+            <h1>Assistive Technologies less than 250</h1>
+            <h2>Please tap a category below</h2>
+        </main>
+    )
+}
