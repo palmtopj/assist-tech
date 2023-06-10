1498474a28b97dbcefa337293c7a773506fe127f
roberto189 <126641489+roberto189@users.noreply.github.com>
2023-06-10 00:17:27 -0700

    added the signin and login pages and connected the API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+// import Login from "./pages/AuthForm"
 import "./app.css";
+import AuthForm from "./pages/AuthForm";
 
 function App() {
   // add stAtes here
@@ -10,12 +12,13 @@ function App() {
       <hr />
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<h2>Login</h2>} />
-        <Route path="/signup" element={<h2>Signup</h2>} />
+        <Route path="/login" element={<AuthForm usage="Login"/>} />
+        <Route path="/signup" element={<AuthForm usage="Signup"/>} />
         <Route path="/category/:id" element={<h2>Devices</h2>} />
         <Route path="/device/:id" element={<h2>Details</h2>} />
         <Route path="/*" element={<h2>404 page not found</h2>} />
     </Routes>
+    <h2>Footer</h2>
     </Router>
   );
 }
