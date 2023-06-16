a493853d4451cf57cae45a0215143a09ca7ec6fe
roberto189 <126641489+roberto189@users.noreply.github.com>
2023-06-15 21:15:33 -0700

    logout button works!

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import "./style.css";
-export default function Navbar() {
+import "../../pages/AuthForm/index";
+export default function Navbar(props) {
+  const [loggedIn, setloggedIn] = useState(false);
+  const handLogout=()=>{
+    setloggedIn(false)
+    localStorage.removeItem("username")
+    window.location.reload()
+  }
+  useEffect(()=>{
+    console.log("changing", props.logout)
+    if (props.username!=="") {
+      console.log(props.username)
+      setloggedIn(true)
+    }
+  },[props.logout])
+  useEffect(()=>{
+    console.log("changing", loggedIn)
+      console.log(props.username)
+      setloggedIn(true)
+  },[loggedIn])
+
+  console.log(props)
   return (
     <nav className='Navbar'>
+      {props.userId>0 ?<span>Welcome, {props.username}!</span>:null}
         <Link to="/">Home</Link> 
-        <Link to="/login">login</Link>
-        <Link to="/signup">signup</Link>
+        {props.username===""?<Link to="/login">login</Link>:null}
+        {props.username===""?<Link to="/signup">signup</Link>:null}
+        {props.username? <button onClick={handLogout}>Logout</button>:"hello"}
+        <h1>{localStorage.getItem("username")}</h1>
     </nav>
   )
 }
